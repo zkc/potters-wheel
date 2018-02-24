cbbe6d19557a4011db7773e4d6bed6552d43e622
zkc <zkyle12@gmail.com>
2018-02-24 10:36:30 -0700

    Begin adding interactivity

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,7 +5,20 @@ import '../Styles/App.css';
 
 // feel the rhythm, feel the rhyme, trust the array indexes 
 const data_lib = {
-  details: {}, // stuff worth saving but no always shown on UI
+  details: {// stuff worth saving but not always shown on UI
+    cards: {
+      9: {
+        created: 'datetime', 
+        edited: [{to: '', from:'', at:'datetime'}, ]
+      }
+    },
+    lists: {
+      1: {
+        created: 'datetime', 
+        edited: ['datetime', 'datetime' ]
+      }
+    }
+  }, 
   lists: [
     {
       id: 1,
@@ -22,15 +35,15 @@ const data_lib = {
         }
       ]
     }, {
-      id: 1,
+      id: 2,
       title: 'Second list, in JSON', 
       cards: [
         {
-          id: 9, 
+          id: 11, 
           title: 'Need CRUD! edit JSON is bla', 
           content: 'Make this into markdown'
         }, {
-          id: 10, 
+          id: 112, 
           title: 'Add more lists', 
           content: 'Make this into markdown'
         }
@@ -43,22 +56,50 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      selected_card: null /// aka card being dragged
+    }
 
+    this.handleClick = this.handleClick.bind(this)
+    this.moveCard = this.moveCard.bind(this)
+  }
+
+  handleClick(e) {
+    const eClass = e.target.className
+    const eId = e.target.id
+    switch (eClass) {
+      case 'card':
+        this.setState({selected_card: eId})
+        break;
+    
+      default:
+        break;
     }
   }
 
+  moveCard() {
+    /// update store
+    console.log('move card', this.state.selected_card)
+  }
+
   render() {
-    const lists = data_lib.lists.map(l => <List {...l}/>) 
+    const lists = data_lib.lists.map(l => <List {...l} key={l.id}/>) 
 
     return (
       <div className="App">
-        <div className="top-bar"></div>
-        <div className="list-space">
+        <div className="top-bar" style={{ textAlign: 'center' }}>
+          {this.state.selected_card}
+          {
+            this.state.selected_card &&
+            <button onClick={this.moveCard}>Move</button>
+          }
+        </div>
+        <div className="list-space" id='list-space' onClick={this.handleClick}>
           {lists}
         </div>
       </div>
     );
   }
 }
+
 
 export default App;
--- src/Components/List.js
+++ src/Components/List.js
@@ -4,9 +4,9 @@ import Card from './Card'
 
 
 const List = (props) => {
-  const cards = props.cards.map(c => <Card {...c} />)
+  const cards = props.cards.map(c => <Card {...c} key={c.id}/>)
   return (
-    <div className="list">
+    <div className="list" id={props.id}>
       <div className="list-title">{props.title}</div>
       <div className="card-space">
         {cards}
