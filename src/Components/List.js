9daf2508d19865c304b8111fff066ed70f3fef72
zkc <zkyle12@gmail.com>
2018-03-25 12:10:30 -0600

    Add drop target to List

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import { DropTarget } from 'react-dnd'
+import { connect } from 'react-redux'
 
 import Card from './Card'
+import types from '../types'
+import { moveCard } from '../modules/lists'
+
+const dropSpec = {
+  // hover(props, monitor) {
+  // check if card or list
+  // }
+}
+
+const dropCollect = (connect, monitor) => {
+  return {
+    connectDropSource: connect.dropTarget()
+  }
+}
 
 
 const List = (props) => {
-  const cards = props.cards.map(c => <Card {...c} current_list={props.id} key={c.id}/>)
-  return (
+  const cards = props.cards.map(c => <Card {...c} current_list={props.id} key={c.id+'card'}/>)
+  return props.connectDropSource(
     <div className="list" id={props.id}>
       <div className="list-title">{props.title}</div>
       <div className="card-space">
@@ -15,4 +31,7 @@ const List = (props) => {
   )
 }
 
-export default List
+const DnDList = DropTarget([types.LIST, types.CARD], dropSpec, dropCollect)(List)
+
+export default connect(null, { moveCard })(DnDList)
+
